3e69d5d0d95c10c18a4e389e65e029054e38ae01
madnattz <madnattz@gmail.com>
2015-09-06 16:24:00 +0700

    merge

---
+++ Solution1.Module/BusinessObjects/Teacher.vb
@@ -0,0 +1,67 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Linq
+Imports System.Text
+Imports DevExpress.Xpo
+Imports DevExpress.ExpressApp
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.DC
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Persistent.Base
+Imports System.Collections.Generic
+Imports DevExpress.ExpressApp.Model
+Imports DevExpress.Persistent.BaseImpl
+Imports DevExpress.Persistent.Validation
+
+<DefaultClassOptions()> _
+Public Class Teacher ' Specify more UI options using a declarative approach (http://documentation.devexpress.com/#Xaf/CustomDocument2701).
+    Inherits BaseObject ' Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (http://documentation.devexpress.com/#Xaf/CustomDocument3146).
+    Public Sub New(ByVal session As Session)
+        MyBase.New(session)
+    End Sub
+    Public Overrides Sub AfterConstruction()
+        MyBase.AfterConstruction()
+        ' Place your initialization code here (http://documentation.devexpress.com/#Xaf/CustomDocument2834).
+    End Sub
+
+    Private _StudentID As String
+    Public Property StudentID() As String
+        Get
+            Return _StudentID
+        End Get
+        Set(ByVal value As String)
+            SetPropertyValue("StudentID", _StudentID, value)
+        End Set
+    End Property
+
+    Private _StudentName As String
+    Public Property StudentName() As String
+        Get
+            Return _StudentName
+        End Get
+        Set(ByVal value As String)
+            SetPropertyValue("StudentName", _StudentName, value)
+        End Set
+    End Property
+
+    Private _Address As String
+    Public Property Address() As String
+        Get
+            Return _Address
+        End Get
+        Set(ByVal value As String)
+            SetPropertyValue("Address", _Address, value)
+        End Set
+    End Property
+
+    Private _Address2 As String
+    Public Property Address2() As String
+        Get
+            Return _Address2
+        End Get
+        Set(ByVal value As String)
+            SetPropertyValue("Address2", _Address2, value)
+        End Set
+    End Property
+
+End Class
