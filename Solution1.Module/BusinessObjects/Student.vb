f4daae55e26b6f5c5c7d7e4be9b61aecdf266f12
madnattz <madnattz@gmail.com>
2015-09-06 14:52:11 +0700

    master create studentName property

---
+++ Solution1.Module/BusinessObjects/Student.vb
@@ -0,0 +1,37 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Linq
+Imports System.Text
+Imports DevExpress.Xpo
+Imports DevExpress.ExpressApp
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.DC
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Persistent.Base
+Imports System.Collections.Generic
+Imports DevExpress.ExpressApp.Model
+Imports DevExpress.Persistent.BaseImpl
+Imports DevExpress.Persistent.Validation
+
+<DefaultClassOptions()> _
+Public Class Student ' Specify more UI options using a declarative approach (http://documentation.devexpress.com/#Xaf/CustomDocument2701).
+    Inherits BaseObject ' Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (http://documentation.devexpress.com/#Xaf/CustomDocument3146).
+    Public Sub New(ByVal session As Session)
+        MyBase.New(session)
+    End Sub
+    Public Overrides Sub AfterConstruction()
+        MyBase.AfterConstruction()
+        ' Place your initialization code here (http://documentation.devexpress.com/#Xaf/CustomDocument2834).
+    End Sub
+
+    Private _StudentName As String
+    Public Property StudentName() As String
+        Get
+            Return _StudentName
+        End Get
+        Set(ByVal value As String)
+            SetPropertyValue("StudentName", _StudentName, value)
+        End Set
+    End Property
+
+End Class
