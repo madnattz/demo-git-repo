641b3f866b9db3a6d58c27bb8f5f38556ad58eaf
chobits2012 <chobits2012@hotmail.co.th>
2015-09-07 13:11:44 +0700

    boy เพื่มclass Test

---
+++ Solution1.Module/BusinessObjects/Test.vb
@@ -0,0 +1,49 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Linq
+Imports System.Text
+Imports DevExpress.Xpo
+Imports DevExpress.ExpressApp
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.DC
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Persistent.Base
+Imports System.Collections.Generic
+Imports DevExpress.ExpressApp.Model
+Imports DevExpress.Persistent.BaseImpl
+Imports DevExpress.Persistent.Validation
+
+'<ImageName("BO_Contact")> _
+'<DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")> _
+'<DefaultListViewOptions(MasterDetailMode.ListViewOnly, False, NewItemRowPosition.None)> _
+'<Persistent("DatabaseTableName")> _
+<DefaultClassOptions()> _
+Public Class Test ' Specify more UI options using a declarative approach (http://documentation.devexpress.com/#Xaf/CustomDocument2701).
+    Inherits BaseObject ' Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (http://documentation.devexpress.com/#Xaf/CustomDocument3146).
+    Public Sub New(ByVal session As Session)
+        MyBase.New(session)
+    End Sub
+    Public Overrides Sub AfterConstruction()
+        MyBase.AfterConstruction()
+        ' Place your initialization code here (http://documentation.devexpress.com/#Xaf/CustomDocument2834).
+    End Sub
+
+    Private _PersistentProperty As String
+    <XafDisplayName("My display name"), ToolTip("My hint message")> _
+    <ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(False)> _
+    <Persistent("DatabaseColumnName"), RuleRequiredField(DefaultContexts.Save)> _
+    Public Property PersistentProperty() As String
+        Get
+            Return _PersistentProperty
+        End Get
+        Set(ByVal value As String)
+            SetPropertyValue("PersistentProperty", _PersistentProperty, value)
+        End Set
+    End Property
+
+    '<Action(Caption:="My UI Action", ConfirmationMessage:="Are you sure?", ImageName:="Attention", AutoCommit:=True)> _
+    'Public Sub ActionMethod()
+    '    ' Trigger a custom business logic for the current record in the UI (http://documentation.devexpress.com/#Xaf/CustomDocument2619).
+    '    Me.PersistentProperty = "Paid"
+    'End Sub
+End Class
