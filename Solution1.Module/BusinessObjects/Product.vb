bdb101d3b0135e35bfa59caa92fd08a35c95899f
panudsakorn <noches_pv@Hotmail.com>
2015-09-07 09:30:20 +0700

    เพิ่ม Product

---
+++ Solution1.Module/BusinessObjects/Product.vb
@@ -0,0 +1,38 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Linq
+Imports System.Text
+Imports DevExpress.Xpo
+Imports DevExpress.ExpressApp
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.DC
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Persistent.Base
+Imports System.Collections.Generic
+Imports DevExpress.ExpressApp.Model
+Imports DevExpress.Persistent.BaseImpl
+Imports DevExpress.Persistent.Validation
+
+
+<DefaultClassOptions()> _
+Public Class Product ' Specify more UI options using a declarative approach (http://documentation.devexpress.com/#Xaf/CustomDocument2701).
+    Inherits BaseObject ' Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (http://documentation.devexpress.com/#Xaf/CustomDocument3146).
+    Public Sub New(ByVal session As Session)
+        MyBase.New(session)
+    End Sub
+    Public Overrides Sub AfterConstruction()
+        MyBase.AfterConstruction()
+        ' Place your initialization code here (http://documentation.devexpress.com/#Xaf/CustomDocument2834).
+    End Sub
+
+    Private fItemNo As String
+    Public Property ItemNo() As String
+        Get
+            Return fItemNo
+        End Get
+        Set(ByVal value As String)
+            SetPropertyValue("ItemNo", fItemNo, value)
+        End Set
+    End Property
+
+End Class
