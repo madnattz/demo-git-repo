f0699f2f5db7b93f8e555a468d100500e281e3a7
chobits2012 <chobits2012@hotmail.co.th>
2015-09-07 13:26:22 +0700

    bot เพิ่ม Property

--- Solution1.Module/BusinessObjects/Test.vb
+++ Solution1.Module/BusinessObjects/Test.vb
@@ -51,6 +51,16 @@ Public Class Test ' Specify more UI options using a declarative approach (http:/
         End Set
     End Property
 
+    Private _TeacherIDBoy As String
+    Public Property TeacherIDBoy() As String
+        Get
+            Return _TeacherIDBoy
+        End Get
+        Set(ByVal value As String)
+            SetPropertyValue("TeacherIDBoy", _TeacherIDBoy, value)
+        End Set
+    End Property
+
     <Action(Caption:="My UI Action", ConfirmationMessage:="Are you sure?", ImageName:="Attention", AutoCommit:=True)> _
     Public Sub ActionMethod()
         ' Trigger a custom business logic for the current record in the UI (http://documentation.devexpress.com/#Xaf/CustomDocument2619).
